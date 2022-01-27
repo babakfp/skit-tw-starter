be4684959f9722a991c3472bf9504187160a4a66
babakfp <babak.bxf@gmail.com>
2022-01-26 16:11:40 -0800

    Added Tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-vercel'
 
 const config = {
-	kit: {
-		target: '#svelte',
-		adapter: adapter(),
-	}
+  kit: {
+    target: '#svelte',
+    adapter: adapter(),
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 }
 
 export default config
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  content: ['./src/**/*.{html,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = config
