d4abdc186be31361b269b6eb4ba8819366686cdc
babakfp <babak.bxf@gmail.com>
2022-01-26 16:24:12 -0800

    New home page UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Hello, Svelte!</h1>
-<a href="https://kit.svelte.dev" target="_blank">docs</a>
+<h1 class="text-9xl text-white">NICE</h1>
+
+<style lang="postcss">
+	@import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+	:global(body) {
+		@apply flex items-center justify-center w-screen h-screen bg-neutral-900;
+		font-family: 'Lilita One';
+	}
+</style>
